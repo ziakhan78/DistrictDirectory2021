5e3303cd1c0e8ed3b667543cede3566bef500aaa
Zia <ziakhan78@gmail.com>
2019-09-19 17:11:25 +0530

    Add Test Method in Home Ctrl

--- DistrictDirectory2021/Controllers/HomeController.cs
+++ DistrictDirectory2021/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace DistrictDirectory2021.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
